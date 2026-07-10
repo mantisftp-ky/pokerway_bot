7e8d5e18eccebce1fa00f35bcd7fd7fe50446ff8
mantisftp-ky <mantisftp@gmail.com>
2026-07-10 13:35:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,31 @@ from telegram.ext import Application, CommandHandler, ContextTypes
 
 TOKEN = os.getenv("BOT_TOKEN")
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [
-        [InlineKeyboardButton("📚 Канал", url="https://t.me/pokerway2020")],
+        [InlineKeyboardButton("📚 Перейти в канал", url="https://t.me/pokerway2020")]
     ]
 
     welcome_text = (
-    "Привет!\n\n"
-    "Добро пожаловать в официальный бот Pokerway.\n\n"
-    "В канале публикуются:\n\n"
-    "— разборы раздач против регуляров и любителей;\n"
-    "— материалы по GTO и эксплойтной стратегии;\n"
-    "— анализ типичных ошибок на постфлопе;\n"
-    "— разборы современных тенденций поля;\n"
-    "— обучающие материалы и комментарии Алексея Заворотного.\n\n"
-    "Если хотите системно развивать игру, нажмите кнопку ниже и перейдите в канал."
-)
+        "Привет!\n\n"
+        "Добро пожаловать в официальный бот Pokerway.\n\n"
+        "В канале публикуются:\n\n"
+        "— разборы раздач;\n"
+        "— материалы по GTO и современным тенденциям поля;\n"
+        "— анализ ошибок на каждой улице;\n"
+        "— обучающие материалы и комментарии Алексея Заворотного.\n\n"
+        "Нажмите кнопку ниже, чтобы перейти в канал."
+    )
 
     await update.message.reply_text(
-        text,
+        welcome_text,
         reply_markup=InlineKeyboardMarkup(keyboard),
     )
 
+
 app = Application.builder().token(TOKEN).build()
+
 app.add_handler(CommandHandler("start", start))
+
 app.run_polling()
